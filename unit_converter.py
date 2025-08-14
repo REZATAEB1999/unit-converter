82bcb3fbc802cff207df0c5c6094e414e7c156a1
REZATAEB1999 <mohammadrezataeb0@gmail.com>
2025-08-14 19:52:15 +0200

    Updated unit_converter.py with improvements

--- unit_converter.py
+++ unit_converter.py
@@ -1,8 +1,8 @@
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
 
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
 
 print("TEMPERATURE CONVERTER")
 choice = input("Convert (1) Celsius to Fahrenheit or (2) Fahrenheit to Celsius? ")
@@ -11,9 +11,12 @@ if choice == "1":
     c = float(input("Enter temperature in Celsius: "))
     f = celsius_to_fahrenheit(c)
     print("Temperature in Fahrenheit:", round(f, 2))
+    
 elif choice == "2":
     f = float(input("Enter temperature in Fahrenheit: "))
     c = fahrenheit_to_celsius(f)
     print("Temperature in Celsius:", round(c, 2))
 else:
     print("Invalid choice.")
+    
+input("Press Enter to exit...")
